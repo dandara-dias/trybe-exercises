41b6d7a0ab6e333d914496c4f981bcac7fdcb249
dandara-dias <dandaradias.contato@gmail.com>
2021-07-03 19:00:32 -0300

    Informa se o valor e maior, menor ou igual a 20

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-2/array-e-for.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-2/array-e-for.js
@@ -1,10 +1,19 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma=0;
+let mediaAritmetica;
 
 for(let i=0; i < numbers.length; i++){
     console.log(numbers[i]);
     soma += numbers[i];
 }
 
+mediaAritmetica = soma/numbers.length;
+
 console.log(soma);
-console.log(soma/numbers.length);
+console.log(mediaAritmetica);
+
+if(mediaAritmetica>20){
+    console.log("valor maior que 20");
+} else{
+    console.log("valor menor ou igual a 20");
+}
