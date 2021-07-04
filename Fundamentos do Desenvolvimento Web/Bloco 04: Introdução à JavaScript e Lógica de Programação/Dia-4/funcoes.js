3f50c621c0b9879642a20820bbaa3630107f36ae
dandara-dias <dandaradias.contato@gmail.com>
2021-07-04 20:40:13 -0300

    Retorna o que mais se repete

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-4/funcoes.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-4/funcoes.js
@@ -50,6 +50,31 @@ function maiorNome(array){
     return console.log(name);
 }
 
+function repete(array){
+    let cont=0, maior=0;
+    let quantidade=[];
+    let index;
+
+    for(let i=0; i<array.length; i++){
+        for(let j=i; j<array.length; j++){
+            if(array[i]==array[j]){
+                cont++;
+            }
+        }
+        quantidade.push(cont);
+        cont=0;
+    }
+
+    for(let i=0; i<quantidade.length; i++){
+        if(maior<quantidade[i]){
+            maior = quantidade[i];
+            index=i;
+        }
+    }
+
+    return console.log(array[index]);
+}
+
 
 ////////////////////////////////////////MAIN//////////////////////////////////////////////////
 
@@ -57,3 +82,4 @@ palindromo("arara");
 indiceMaiorValor([2, 3, 6, 7, 10, 1]);
 indiceMenorValor([2, 4, 6, 7, 10, 0, -3]);
 maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
+repete([2, 3, 2, 5, 8, 2, 3]);
