b40a6c615cd1aa3774a030e42beb3fb5df403ee6
dandara-dias <dandaradias.contato@gmail.com>
2021-07-04 01:07:35 -0300

    Tira um for

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-3/triangulo.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-3/triangulo.js
@@ -4,8 +4,6 @@ let str;
 
 for(let i=0; i<n; i++){
     array.push("*");
-    for(let j=0; j<n; j++){
-        str = array.join("");   
-    }
+    str = array.join("");   
     console.log(str);
 }
