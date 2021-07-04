feb0e57985861277903957535ab7889949f295c7
dandara-dias <dandaradias.contato@gmail.com>
2021-07-04 19:07:04 -0300

    Retorna indice de maior valor

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-4/funcoes.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/Dia-4/funcoes.js
@@ -7,13 +7,26 @@ function palindromo(string){
     }
     str = array.join("");
     if(str === string){
-        console.log("true");
+        return console.log("true");
     } else{
-        console.log("false");
+        return console.log("false");
     }
+}
+
+function indiceMaiorValor(array){
+    let maior=0, index;
+
+    for(let i=0; i<array.length; i++){
+        if(array[i]>maior){
+            maior = array[i];
+            index = i;
+        }
+    }
+    return console.log(index);
 }
 
 
 ////////////////////////////////////////MAIN//////////////////////////////////////////////////
 
 palindromo("arara");
+indiceMaiorValor([2, 3, 6, 7, 10, 1]);
