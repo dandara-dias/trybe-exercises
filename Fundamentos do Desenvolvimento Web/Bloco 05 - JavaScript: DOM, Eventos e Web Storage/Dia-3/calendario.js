d6d0b06f7639de3b062b36cad9dfc638d24bdf1e
dandara-dias <dandaradias.contato@gmail.com>
2021-07-17 03:31:46 -0300

    Exercicio 1 - coloca dias

--- Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-3/calendario.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-3/calendario.js
@@ -14,3 +14,27 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function criaDias(){
+
+    for(let i=0; i<dezDaysList.length; i++){
+        let dia = dezDaysList[i];
+        let day = document.createElement("li");
+        day.innerHTML = dia;
+        days.appendChild(day);
+
+        if(dia==24 || dia==31){
+            day.className = "day holiday";
+        } else if(dia==4 || dia==11 || dia==18){
+            day.className = "day friday";
+        } else if(dia==25){
+            day.className = "day holiday friday";
+        } else{
+            day.className = "day";
+        }
+    }
+}
+
+criaDias();
