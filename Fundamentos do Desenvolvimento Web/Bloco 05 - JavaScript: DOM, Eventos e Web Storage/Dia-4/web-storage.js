0b569fb02dbff17a5d3bdb07b720d257f7551983
dandara-dias <dandaradias.contato@gmail.com>
2021-07-27 01:21:16 -0300

    Muda o espaçamento

--- Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
@@ -2,10 +2,12 @@ let bg = document.querySelector("#bg");
 let clr = document.querySelector("#clr");
 let p = document.querySelector("p");
 let size = document.querySelector("#ft-sz");
+let height = document.querySelector("#ft-ht");
 
 bg.addEventListener("click", setBackgroundColor);
 clr.addEventListener("click", setColor);
 size.addEventListener("click", setSize);
+height.addEventListener("click", setHeight);
 
 function setBackgroundColor(event){
     let color = event.target.value;
@@ -20,4 +22,9 @@ function setColor(event){
 function setSize(event){
   let size = event.target.value;
   p.style.fontSize = `${size}px`;
+}
+
+function setHeight(event){
+  let height = event.target.value;
+  p.style.lineHeight = height;
 }
