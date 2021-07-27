39acfe01c6e3dd46921031df88c443ed9b388683
dandara-dias <dandaradias.contato@gmail.com>
2021-07-27 01:16:39 -0300

    Muda tamanho da fonte

--- Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
@@ -1,9 +1,11 @@
 let bg = document.querySelector("#bg");
 let clr = document.querySelector("#clr");
 let p = document.querySelector("p");
+let size = document.querySelector("#ft-sz");
 
 bg.addEventListener("click", setBackgroundColor);
 clr.addEventListener("click", setColor);
+size.addEventListener("click", setSize);
 
 function setBackgroundColor(event){
     let color = event.target.value;
@@ -13,4 +15,9 @@ function setBackgroundColor(event){
 function setColor(event){
   let color = event.target.value;
   p.style.color = color;
+}
+
+function setSize(event){
+  let size = event.target.value;
+  p.style.fontSize = `${size}px`;
 }
