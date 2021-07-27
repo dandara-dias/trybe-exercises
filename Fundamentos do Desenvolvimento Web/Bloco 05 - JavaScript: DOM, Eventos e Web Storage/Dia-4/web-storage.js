14ca037a21d50937d6b4dc3c608ff1991847a5e3
dandara-dias <dandaradias.contato@gmail.com>
2021-07-27 01:33:02 -0300

    Salva os dados em local storage

--- Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
@@ -12,19 +12,37 @@ height.addEventListener("click", setHeight);
 function setBackgroundColor(event){
     let color = event.target.value;
     document.body.style.backgroundColor = color;
+
+    localStorage.setItem("5.4-background-color", color);
 }
 
 function setColor(event){
   let color = event.target.value;
   p.style.color = color;
+
+  localStorage.setItem("5.4-color", color);
 }
 
 function setSize(event){
   let size = event.target.value;
   p.style.fontSize = `${size}px`;
+
+  localStorage.setItem("5.4-font-size", `${size}px`);
 }
 
 function setHeight(event){
   let height = event.target.value;
   p.style.lineHeight = height;
+
+  localStorage.setItem("5.4-line-height", height);
 }
+
+let savedBg = localStorage.getItem("5.4-background-color");
+let savedColor = localStorage.getItem("5.4-color");
+let savedSize = localStorage.getItem("5.4-font-size");
+let savedHeight = localStorage.getItem("5.4-line-height");
+
+document.body.style.backgroundColor = savedBg;
+p.style.color = savedColor;
+p.style.fontSize = savedSize;
+p.style.lineHeight = savedHeight;
