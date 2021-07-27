e61c5bdd1d2c39e0334b8e16c08b46d9ed190e2b
dandara-dias <dandaradias.contato@gmail.com>
2021-07-27 01:01:12 -0300

    Muda a cor do texto

--- Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/Dia-4/web-storage.js
@@ -1,8 +1,16 @@
 let bg = document.querySelector("#bg");
+let clr = document.querySelector("#clr");
+let p = document.querySelector("p");
 
-bg.addEventListener("click", setBackgroundColor)
+bg.addEventListener("click", setBackgroundColor);
+clr.addEventListener("click", setColor);
 
-function setBackgroundColor(event) {
+function setBackgroundColor(event){
     let color = event.target.value;
     document.body.style.backgroundColor = color;
+}
+
+function setColor(event){
+  let color = event.target.value;
+  p.style.color = color;
 }
