f7933da1cff598b613b9608035de342864f05318
dandara-dias <dandaradias.contato@gmail.com>
2021-08-02 02:40:51 -0300

    Dados do ultimo emprego

--- Fundamentos do Desenvolvimento Web/Bloco 06 - HTML e CSS: Forms, Flexbox e Responsivo/Dia-1/forms.js
+++ Fundamentos do Desenvolvimento Web/Bloco 06 - HTML e CSS: Forms, Flexbox e Responsivo/Dia-1/forms.js
@@ -1,5 +1,4 @@
 let estados = ["AC", "AL", "AP", "AM", "BA", "CE", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO", "DF"];
-
 let select = document.getElementById("estado");
 
 for(let i=0; i<estados.length; i++){
