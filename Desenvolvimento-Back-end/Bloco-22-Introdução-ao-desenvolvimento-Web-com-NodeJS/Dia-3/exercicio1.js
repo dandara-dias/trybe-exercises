4834fc539c03515ee5feb9a0233ff12e37c7180e
dandara-dias <dandaradias.contato@gmail.com>
2022-01-11 16:15:41 -0300

    Exercicio 1.3 - cria as asserçoes

--- Desenvolvimento-Back-end/Bloco-22-Introdução-ao-desenvolvimento-Web-com-NodeJS/Dia-3/exercicio1.js
+++ Desenvolvimento-Back-end/Bloco-22-Introdução-ao-desenvolvimento-Web-com-NodeJS/Dia-3/exercicio1.js
@@ -1,29 +1,45 @@
+const { expect } = require('chai');
+
+const funcaoSinal = require('./sinal');
+
 describe('Quando o número for maior que 0', () => {
   it('A resposta é uma string', () => {
-    //
+    const resposta = funcaoSinal(1);
+
+    expect(resposta).to.be.a('string');
   });
 
   it('Retorna "positivo"', () => {
-    //
+    const resposta = funcaoSinal(1);
+
+    expect(resposta).to.be.equals('positivo');
   });
 });
 
 describe('Quando o número for menor que 0', () => {
   it('A resposta é uma string', () => {
-    //
+    const resposta = funcaoSinal(-1);
+
+    expect(resposta).to.be.a('string');
   });
 
   it('Retorna "negativo"', () => {
-    //
+    const resposta = funcaoSinal(-1);
+
+    expect(resposta).to.be.equals('negativo');
   });
 });
 
 describe('Quando o número for igual a 0', () => {
   it('A resposta é uma string', () => {
-    //
+    const resposta = funcaoSinal(0);
+
+    expect(resposta).to.be.a('string');
   });
 
   it('Retorna "neutro"', () => {
-    //
+    const resposta = funcaoSinal(0);
+
+    expect(resposta).to.be.equals('neutro');
   });
 });
