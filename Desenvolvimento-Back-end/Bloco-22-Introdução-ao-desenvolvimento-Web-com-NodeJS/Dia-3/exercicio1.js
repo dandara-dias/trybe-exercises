af78a4b952a908f36f324fc8f3cf8905551a85ab
dandara-dias <dandaradias.contato@gmail.com>
2022-01-11 15:58:37 -0300

    Exercicio 1.1 - descreve todos os cenarios de teste utilizando describe

---
+++ Desenvolvimento-Back-end/Bloco-22-Introdução-ao-desenvolvimento-Web-com-NodeJS/Dia-3/exercicio1.js
@@ -0,0 +1,11 @@
+describe('Quando o número for maior que 0', () => {
+  //
+});
+
+describe('Quando o número for menor que 0', () => {
+  //
+});
+
+describe('Quando o número for igual a 0', () => {
+  //
+});
