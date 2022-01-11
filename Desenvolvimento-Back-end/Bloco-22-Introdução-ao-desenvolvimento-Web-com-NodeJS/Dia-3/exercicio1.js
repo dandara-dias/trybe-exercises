f511f01ce299134093c0f83d789fb69a002b9361
dandara-dias <dandaradias.contato@gmail.com>
2022-01-11 16:08:31 -0300

    Exercicio 1.2 - descreve todos os testes que serao feitos utilizando it

--- Desenvolvimento-Back-end/Bloco-22-Introdução-ao-desenvolvimento-Web-com-NodeJS/Dia-3/exercicio1.js
+++ Desenvolvimento-Back-end/Bloco-22-Introdução-ao-desenvolvimento-Web-com-NodeJS/Dia-3/exercicio1.js
@@ -1,11 +1,29 @@
 describe('Quando o número for maior que 0', () => {
-  //
+  it('A resposta é uma string', () => {
+    //
+  });
+
+  it('Retorna "positivo"', () => {
+    //
+  });
 });
 
 describe('Quando o número for menor que 0', () => {
-  //
+  it('A resposta é uma string', () => {
+    //
+  });
+
+  it('Retorna "negativo"', () => {
+    //
+  });
 });
 
 describe('Quando o número for igual a 0', () => {
-  //
+  it('A resposta é uma string', () => {
+    //
+  });
+
+  it('Retorna "neutro"', () => {
+    //
+  });
 });
