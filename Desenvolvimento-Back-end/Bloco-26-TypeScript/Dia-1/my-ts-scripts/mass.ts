a6374ad51e115d83ad03672ae21dad258e624fd3
dandara-dias <dandaradias.contato@gmail.com>
2022-02-17 15:58:55 -0300

    Exercicio 7

--- Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-1/my-ts-scripts/mass.ts
+++ Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-1/my-ts-scripts/mass.ts
@@ -1,9 +1,26 @@
+import readline from 'readline-sync';
+
 const units = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
 
-export function convert(value: number, base: string, convertion: string): number {
+function convert(value: number, base: string, convertion: string): number {
   const from = units.indexOf(base);
   const to = units.indexOf(convertion);
   const exp = (to - from);
 
   return value * Math.pow(10, exp);
 }
+
+function exec() {
+  const value = readline.questionFloat('Digite o valor a ser convertido: ');
+  const from = readline.keyInSelect(units, 'Escolha um número para a unidade base: ');
+  const to = readline.keyInSelect(units, 'Escolha um número para a conversão: ');
+
+  const base = units[from];
+  const convertion = units[to];
+
+  const result = convert(value, base, convertion);
+
+  console.log(`${value}${from} é igual a ${result}${to}`);
+}
+
+exec();
