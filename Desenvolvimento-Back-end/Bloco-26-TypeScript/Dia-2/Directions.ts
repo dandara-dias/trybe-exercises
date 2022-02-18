45bae4e622c67a736bc25ffec6dc4ea19becd582
dandara-dias <dandaradias.contato@gmail.com>
2022-02-18 18:24:27 -0300

    Exercicio 4

---
+++ Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/Directions.ts
@@ -0,0 +1,6 @@
+enum Directions {
+  left = 'left',
+  right = 'right',
+}
+
+export default Directions;
---
+++ Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/Doors.ts
@@ -0,0 +1,8 @@
+enum Doors {
+  driver = 'Da pessoa motorista',
+  ride = 'Da pessoa carona',
+  behind_driver = 'De trás da pessoa motorista',
+  behind_ride = 'De trás da pessoa carona',
+}
+
+export default Doors;
