51f96f0362e7289704091372b9f9042a09a7fb24
dandara-dias <dandaradias.contato@gmail.com>
2022-02-18 17:23:40 -0300

    Exercicio 1

---
+++ Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/months.ts
@@ -0,0 +1,15 @@
+enum months {
+  janeiro = 'january',
+  fevereiro = 'february',
+  março = 'march',
+  abril = 'april',
+  maio = 'may',
+  junho = 'june',
+  agosto = 'august',
+  setembro = 'september',
+  outubro = 'october',
+  novembro = 'november',
+  dezembro = 'december',
+}
+
+export default months;
