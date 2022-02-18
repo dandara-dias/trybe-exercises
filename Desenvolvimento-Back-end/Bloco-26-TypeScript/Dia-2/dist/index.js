1476ef2a288a3dc008e73eee9a3f0ed991f64c6e
dandara-dias <dandaradias.contato@gmail.com>
2022-02-18 19:20:12 -0300

    Exercicio 6

--- Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/dist/index.js
+++ Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/dist/index.js
@@ -72,3 +72,18 @@ gol.stop(); // Você chegou ao destino.
 gol.openTheDoor(Doors_1.default.behind_ride); // A pessoa passageira desce do veículo
 gol.closeTheDoor(Doors_1.default.behind_ride);
 gol.speedUp(); //  a pessoa motorista segue para a próxima corrida.
+const calabresa = {
+    flavor: 'Calabresa',
+    slices: 8,
+};
+console.log(calabresa);
+const marguerita = {
+    flavor: 'Marguerita',
+    slices: 6,
+};
+console.log(marguerita);
+const nutella = {
+    flavor: 'Nutella',
+    slices: 4,
+};
+console.log(nutella);
