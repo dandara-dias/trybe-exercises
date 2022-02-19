591869b5e68cff44aa25c0015b5eb6ec164f11bc
dandara-dias <dandaradias.contato@gmail.com>
2022-02-18 21:52:46 -0300

    Exercicio 8

--- Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/dist/index.js
+++ Desenvolvimento-Back-end/Bloco-26-TypeScript/Dia-2/dist/index.js
@@ -117,3 +117,18 @@ const marshmallow = {
     slices: 4,
 };
 console.log(marshmallow);
+function myFilter(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+console.log(myFilter([1, 2, 3], (item) => {
+    return item < 3;
+}));
+console.log(myFilter(['a', 'b', 'c'], (item) => {
+    return item !== 'a';
+}));
