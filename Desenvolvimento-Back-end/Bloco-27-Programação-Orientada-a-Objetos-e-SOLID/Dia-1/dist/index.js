64a85805d98afc6e8e31e409ce39d3f30048f291
dandara-dias <dandaradias.contato@gmail.com>
2022-03-03 15:42:26 -0300

    Exercicio 5

--- Desenvolvimento-Back-end/Bloco-27-Programação-Orientada-a-Objetos-e-SOLID/Dia-1/dist/index.js
+++ Desenvolvimento-Back-end/Bloco-27-Programação-Orientada-a-Objetos-e-SOLID/Dia-1/dist/index.js
@@ -60,83 +60,115 @@
 // console.log(student1);
 // console.log('Soma de todas as notas: ', student1.notasSum());
 // console.log('Média de todas as notas: ', student1.notasAverage());
-class Client {
-    constructor(n) {
-        this.name = n;
+// class Client {
+//   private _name!: string;
+//   constructor(n: string) {
+//     this.name = n;
+//   }
+//   get name(): string {
+//     return this._name;
+//   }
+//   set name(value: string) {
+//     this._name = value;
+//   }
+// }
+// class OrderItem {
+//   private _name!: string;
+//   private _price!: number;
+//   constructor(n: string, p: number) {
+//     this.name = n;
+//     this.price = p;
+//   }
+//   get name(): string {
+//     return this._name;
+//   }
+//   set name(value: string) {
+//     this._name = value;
+//   }
+//   get price(): number {
+//     return this._price;
+//   }
+//   set price(value: number) {
+//     this._price = value;
+//   }
+// }
+// class Order {
+//   private _client: Client;
+//   private _items: OrderItem[] = [];
+//   private _paymentMethod: string;
+//   private _discount: number = 0;
+//   constructor(c: Client, i: OrderItem[], p: string, d: number) {
+//     this._client = c;
+//     this.items = i;
+//     this._paymentMethod = p;
+//     this.discount = d;
+//   }
+//   get client(): Client {
+//     return this._client;
+//   }
+//   set client(value: Client) {
+//     this._client = value;
+//   }
+//   get items(): OrderItem[] {
+//     return this._items;
+//   }
+//   set items(value: OrderItem[]) {
+//     this._items = value;
+//   }
+//   get paymentMethod(): string {
+//     return this._paymentMethod;
+//   }
+//   set paymentMethod(value: string) {
+//     this._paymentMethod = value;
+//   }
+//   get discount(): number {
+//     return this._discount;
+//   }
+//   set discount(value: number) {
+//     this._discount = value;
+//   }
+//   totalOrder(): number {
+//     return this.items.reduce((previousPrice, item) => {
+//       const total = previousPrice += item.price;
+//       return total;
+//     }, 0);
+//   }
+//   discountOrder(): number {
+//     return this.totalOrder() * (1 - this.discount);
+//   }
+// }
+// const client1 = new Client('João');
+// const item1 = new OrderItem('Sanduíche Natural', 5.00);
+// const item2 = new OrderItem('Suco de Abacaxi', 5.00);
+// const item3 = new OrderItem('Gelatina de Uva', 2.50);
+// const order1 = new Order(client1, [item1, item2, item3], 'dinheiro', 0.10);
+// console.log(order1);
+// console.log('Total do pedido: ', order1.totalOrder());
+// console.log('Pedido com desconto: ', order1.discountOrder());
+class Data {
+    constructor(d, m, y) {
+        this._day = d;
+        this._month = m;
+        this._year = y;
     }
-    get name() {
-        return this._name;
+    get day() {
+        return this._day;
     }
-    set name(value) {
-        this._name = value;
+    set day(value) {
+        this._day = value;
+    }
+    get month() {
+        return this._month;
+    }
+    set month(value) {
+        this._month = value;
+    }
+    get year() {
+        return this._year;
+    }
+    set year(value) {
+        this._year = value;
     }
 }
-class OrderItem {
-    constructor(n, p) {
-        this.name = n;
-        this.price = p;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
-    get price() {
-        return this._price;
-    }
-    set price(value) {
-        this._price = value;
-    }
-}
-class Order {
-    constructor(c, i, p, d) {
-        this._items = [];
-        this._discount = 0;
-        this._client = c;
-        this.items = i;
-        this._paymentMethod = p;
-        this.discount = d;
-    }
-    get client() {
-        return this._client;
-    }
-    set client(value) {
-        this._client = value;
-    }
-    get items() {
-        return this._items;
-    }
-    set items(value) {
-        this._items = value;
-    }
-    get paymentMethod() {
-        return this._paymentMethod;
-    }
-    set paymentMethod(value) {
-        this._paymentMethod = value;
-    }
-    get discount() {
-        return this._discount;
-    }
-    set discount(value) {
-        this._discount = value;
-    }
-    totalOrder() {
-        return this.items.reduce((previousPrice, item) => {
-            const total = previousPrice += item.price;
-            return total;
-        }, 0);
-    }
-    discountOrder() {
-        return this.totalOrder() * (1 - this.discount);
-    }
-}
-const client1 = new Client('João');
-const item1 = new OrderItem('Sanduíche Natural', 5.00);
-const item2 = new OrderItem('Suco de Abacaxi', 5.00);
-const item3 = new OrderItem('Gelatina de Uva', 2.50);
-const order1 = new Order(client1, [item1, item2, item3], 'dinheiro', 0.10);
-console.log(order1);
-console.log('Total do pedido: ', order1.totalOrder());
-console.log('Pedido com desconto: ', order1.discountOrder());
+const date1 = new Data(3, 3, 2022);
+console.log(date1);
