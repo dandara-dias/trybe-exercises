75fb15d233a90e9cf958baa877dffaa9aa71a504
dandara-dias <dandaradias.contato@gmail.com>
2022-03-04 16:54:24 -0300

    Exercicio 2

---
+++ Desenvolvimento-Back-end/Bloco-27-Programação-Orientada-a-Objetos-e-SOLID/Dia-2/dist/Student.js
@@ -0,0 +1,58 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Person_1 = __importDefault(require("./Person"));
+class Student extends Person_1.default {
+    constructor(name, birthDate) {
+        super(name, birthDate);
+        this._matricula = String();
+        this._notasProva = [];
+        this._notasTrabalho = [];
+        this.matricula = this.geraMatricula();
+    }
+    get matricula() {
+        return this._matricula;
+    }
+    set matricula(value) {
+        if (value.length < 16) {
+            throw new Error('A matrícula deve possuir no mínimo 16 caracteres.');
+        }
+        this._matricula = value;
+    }
+    get notasProva() {
+        return this._notasProva;
+    }
+    set notasProva(value) {
+        if (value.length > 4) {
+            throw new Error('A pessoa estudante só pode possuir 4 notas de provas');
+        }
+        this._notasProva = value;
+    }
+    get notasTrabalho() {
+        return this._notasTrabalho;
+    }
+    set notasTrabalho(value) {
+        if (value.length > 2) {
+            throw new Error('A pessoa estudante só pode possuir 2 notas de trabalhos');
+        }
+        this._notasTrabalho = value;
+    }
+    notasSum() {
+        return [...this.notasProva, ...this.notasTrabalho].reduce((previousGrade, grade) => {
+            grade += previousGrade;
+            return grade;
+        }, 0);
+    }
+    notasAverage() {
+        const sumGrades = this.notasSum();
+        const divider = this.notasProva.length + this.notasTrabalho.length;
+        return Math.round(sumGrades / divider);
+    }
+    geraMatricula() {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+        return `STU${randomStr}`;
+    }
+}
+exports.default = Student;
--- Desenvolvimento-Back-end/Bloco-27-Programação-Orientada-a-Objetos-e-SOLID/Dia-2/index.ts
+++ Desenvolvimento-Back-end/Bloco-27-Programação-Orientada-a-Objetos-e-SOLID/Dia-2/index.ts
@@ -1,7 +1,26 @@
-import Person from "./Person";
+// import Person from "./Person";
 
-const pessoa1 = new Person('Pessoa Um', new Date('1980/01/25'));
-const pessoa2 = new Person('Pessoa Dois', new Date('2005/10/02'));
+// const pessoa1 = new Person('Pessoa Um', new Date('1980/01/25'));
+// const pessoa2 = new Person('Pessoa Dois', new Date('2005/10/02'));
 
-console.log(pessoa1);
-console.log(pessoa2);
+// console.log(pessoa1);
+// console.log(pessoa2);
+
+import Student from "./Student";
+
+const student1 = new Student('Estudante Um', new Date('2005/03/17'));
+const student2 = new Student('Estudante Dois', new Date('2006/07/19'));
+const student3 = new Student('Estudante Três', new Date('2004/06/06'));
+const student4 = new Student('Estudante Quatro', new Date('2005/11/05'));
+const student5 = new Student('Estudante Cinco', new Date('2006/09/11'));
+
+student1.notasProva = [25, 20, 25, 23];
+student2.notasProva = [25, 20, 25, 23];
+student3.notasTrabalho = [50, 45];
+student4.notasTrabalho = [47, 42];
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
+console.log(student4);
+console.log(student5);
